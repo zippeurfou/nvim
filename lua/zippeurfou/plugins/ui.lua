bda06c7a96d05fc72872ab8142af8724ec480ba0
Marc Ferradou <mferradou@grubhub.com>
2023-05-23 15:40:08 -0400

    Move to <leader>T for Telescope

--- lua/zippeurfou/plugins/ui.lua
+++ lua/zippeurfou/plugins/ui.lua
@@ -18,8 +18,35 @@ return {
   },
   { 'Pocco81/true-zen.nvim',
     config = function()
-     require("true-zen").setup {
-    }
+      require("true-zen").setup {
+      }
+    end,
+  },
+  -- {
+  --   "luukvbaal/statuscol.nvim", config = function()
+  --     -- need to figure out how to make it work
+  --     -- local builtin = require("statuscol.builtin")
+  --     require("statuscol").setup({
+  --       -- configuration goes here, for example:
+  --       -- relculright = true,
+  --       segments = {
+  --         { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+  --         {
+  --           sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+  --           click = "v:lua.ScSa"
+  --         },
+  --         -- { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+  --       --   {
+  --           -- sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+  --       --     click = "v:lua.ScSa"
+  --       --   },
+  --       }
+  --     })
+  --   end,
+  -- },
+  { 'chentoast/marks.nvim',
+    config = function()
+      require 'marks'.setup {}
     end,
   },
   { 'lukas-reineke/indent-blankline.nvim',
